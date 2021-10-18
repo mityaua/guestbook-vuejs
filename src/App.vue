9a635e69f2ec1813e8facb69ad11f983a1c1f51b
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-10-18 21:15:17 +0300

    Added first simple custom component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Title></Title>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Title
   }
 }
 </script>
---
+++ src/components/Title.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>H2 Title Example (new component)</h2>
+  <h3>H2 Title Example (same component)</h3>
+</template>
