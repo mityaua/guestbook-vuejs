76aae55f6158d9e641e34dceb127bedbf2fecb2a
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-10-21 20:59:31 +0300

    Правит скрипт для деплоя

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:mityaua/mityaua.github.io.git master
+git push -f git@github.com:mityaua/mityaua.github.io.git main
 cd -
