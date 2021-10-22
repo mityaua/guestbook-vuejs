c7eb902fd94cfb111535866dc7759e96ca21ef87
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-10-22 21:51:26 +0300

    Правит конфиг файл для деплоя

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/guestbook-vuejs/' : '/',
+  publicPath: '/guestbook-vuejs/',
 };
